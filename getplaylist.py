e699be73e1a2cfd7b20281a81dd2d5614740568b
tongxunlu <1849141+tongxunlu@users.noreply.github.com>
2024-10-28 09:26:01 +0800

    Update and rename ott.py to getplaylist.py

---
+++ getplaylist.py
@@ -0,0 +1,69 @@
+import json
+import time
+import requests
+
+uid = os.getenv('UID')
+json_file = 'playlist'
+
+channel_map = json.loads( os.getenv('ROOTIDS') )
+
+root_ids = {}
+
+import json
+import requests
+
+def get_channel_url(channel_id):
+    # 分割 ID 以处理可能的后缀
+    temp_id = channel_id.split("-")
+    base_id = temp_id[0]  # 基础 ID (例如: "Live1704966749996185")
+    temp_index = 1 if len(temp_id) > 1 else 0  # 根据后缀的存在确定索引
+
+    # 请求的 URL 和数据
+    url = os.getenv('LIVE_API')
+    post_data = {
+        "rate": "",
+        "systemType": "android",
+        "model": "Sony-BRAVIA 4K",
+        "id": base_id,
+        "userId": "",
+        "clientSign": "cctvVideo",
+        "deviceId": {
+            "serial": "",
+            "imei": "",
+            "android_id": ""
+        }
+    }
+
+    headers = json.loads( os.getenv('HEADERS') )
+
+    # 发送 POST 请求
+    response = requests.post(url, json=post_data, headers=headers)
+
+    # 解析 JSON 响应
+    if response.status_code == 200:
+        obj = response.json()
+
+        # 检查响应中是否包含 URL
+        video_list = obj.get('data', {}).get('videoList', [])
+        if video_list:  # 如果 videoList 不为空
+            return video_list[temp_index]['url']  # 返回对应索引的播放 URL
+
+    return "bad req"  # 如果未找到 URL，返回错误消息
+
+
+# Clear the JSON file
+with open(json_file, 'w') as f:
+    f.write("")
+
+# Fetch channel URLs
+for channel, live_id in channel_map.items():
+    root_id = get_channel_url(live_id)
+    root_ids[channel] = root_id
+    time.sleep(0.5)  # Set request interval (0.5 seconds)
+
+# Write the JSON data to file
+with open(json_file, 'w') as f:
+    json.dump(root_ids, f)
+
+# Print the JSON data
+print(json.dumps(root_ids))
